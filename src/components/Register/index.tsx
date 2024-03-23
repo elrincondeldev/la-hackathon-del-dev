68ba7e449945c5332997d81698de3dde36b11493
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2024-03-23 22:46:02 +0100

    build correction

--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -26,6 +26,11 @@ const DEFAULT_REGISTER_FORM_VALUES: RegisterForm = {
 export const Register = () => {
   const supabaseUrl = process.env.VITE_SUPABASE_URL
   const supabaseKey = process.env.VITE_SUPABASE_KEY
+
+  if (!supabaseUrl || !supabaseKey) {
+    throw new Error('Supabase URL or Supabase Key is not defined')
+  }
+
   const supabase = createClient(supabaseUrl, supabaseKey)
 
   const {
