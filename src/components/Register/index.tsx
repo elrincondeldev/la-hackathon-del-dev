13144aa4f7d9974ce1cb1395eeda9d5cbcdaec14
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2024-03-23 22:21:12 +0100

    Update index.tsx

--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -24,8 +24,8 @@ const DEFAULT_REGISTER_FORM_VALUES: RegisterForm = {
 } as const
 
 export const Register = () => {
-  const supabaseUrl = import.meta.env.SUPABASE_URL
-  const supabaseKey = import.meta.env.SUPABASE_KEY
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
   const supabase = createClient(supabaseUrl, supabaseKey)
 
   const {
