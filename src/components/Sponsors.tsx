e8cdd3ef4b06d7b9f828fc63d743b3c0bd86dfb0
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2024-04-03 14:50:41 +0200

    added other links

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -8,18 +8,24 @@ export const Sponsors = () => {
     >
       <Title>Patrocinadores</Title>
       <section className="grid md:grid-cols-[1fr_0.5fr_1fr_1fr] place-items-center gap-32 w-full">
-        <img src="/images/sponsors/logitech.svg" alt="logitech logo" />
-        <img
-          src="/images/sponsors/getmagical_green.svg"
-          alt="getmagical logo"
-        />
+        <a href="https://www.logitechg.com/es-es">
+          <img src="/images/sponsors/logitech.svg" alt="logitech logo" />
+        </a>
+        <a href="https://www.getmagical.com/">
+          <img
+            src="/images/sponsors/getmagical_green.svg"
+            alt="getmagical logo"
+          />
+        </a>
         <a
           href="https://4geeksacademy.com/es/inicio?utm_source=influencers&utm_medium=referral&utm_campaign=elrincondeldev&utm_location=madrid-spain&utm_content=landing"
           target="_blank"
         >
           <img src="/images/sponsors/4geeks.svg" alt="4geeks logo" />
         </a>
-        <img src="/images/sponsors/nuwe.svg" alt="nuwe logo" />
+        <a href="https://nuwe.io/">
+          <img src="/images/sponsors/nuwe.svg" alt="nuwe logo" />
+        </a>
       </section>
     </article>
   )
